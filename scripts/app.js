7b85d1b255958bd9a07a110f41a3cee93809013b
monkey-d-luffy <ktkopp@icloud.com>
2022-02-07 16:14:50 -0700

    o

--- scripts/app.js
+++ scripts/app.js
@@ -6,45 +6,205 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
-
 class Player {
-    constructor(){
-        this.width = 32;
-        this.height = 32;
-        this.x = (canvas.width / 2) - (this.width / 2);
-        this.y = (canvas.height / 2) - (this.height / 2);
+	constructor() {
+		this.width = 32;
+		this.height = 32;
+		this.x = canvas.width / 2 - this.width / 2;
+		this.y = canvas.height / 2 - this.height / 2;
 
-        this.wireUpEvents();
-    }
+		this.isMovingUp = false;
+		this.isMovingDown = false;
+		this.isMovingRight = false;
+		this.isMovingLeft = false;
+		this.isSneaking = false;
+		this.isRunning = false;
 
-    wireUpEvents() {
-        window.addEventListener("keydown", (e) => {
-            console.log(e.key);
-        });
-    }
+		this.baseSpeed = 3;
 
-    update() {}
+		this.wireUpEvents();
+	}
 
-    render() {
-        ctx.save();
-        ctx.fillStyle = "teal";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.restore();
-    }
+	wireUpEvents() {
+		window.addEventListener("keydown", (e) => {
+			// console.log(e.key);
+			this.toggleMovement(e.key, true);
+		});
+
+		window.addEventListener("keyup", (e) => {
+			// console.log(e.key);
+			this.toggleMovement(e.key, false);
+		});
+	}
+
+	toggleMovement(key, toggleValue) {
+		switch (key) {
+			case "ArrowUp":
+			case "w":
+				this.isMovingUp = toggleValue;
+				break;
+			case "ArrowDown":
+			case "s":
+				this.isMovingDown = toggleValue;
+				break;
+			case "ArrowRight":
+			case "d":
+				this.isMovingRight = toggleValue;
+				break;
+			case "ArrowLeft":
+			case "a":
+				this.isMovingLeft = toggleValue;
+				break;
+			case "Control":
+				this.isSneaking = toggleValue;
+				break;
+			case "Shift":
+				this.isRunning = toggleValue;
+				break;
+		}
+	}
+
+	update(elapsedTime) {
+		let speedMultiplier = 1;
+
+		if (this.isRunning && !this.isSneaking) {
+			speedMultiplier = 2;
+		} else if (this.isSneaking && !this.isRunning) {
+			speedMultiplier = 0.5;
+		}
+
+		let speed = this.baseSpeed * speedMultiplier;
+
+		if (this.isMovingUp) {
+			this.y -= speed;
+		}
+
+		if (this.isMovingDown) {
+			this.y += speed;
+		}
+
+		if (this.isMovingRight) {
+			this.x += speed;
+		}
+
+		if (this.isMovingLeft) {
+			this.x -= speed;
+		}
+
+		if (this.x + this.width >= canvas.width) {
+			this.x = canvas.width - this.width;
+		}
+		if (this.x <= 0) {
+			this.x = 0;
+		}
+		if (this.y + this.height >= canvas.height) {
+			this.y = canvas.height - this.height;
+		}
+		if (this.y <= 0) {
+			this.y = 0;
+		}
+	}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
 }
+
+class Monster {
+	constructor() {
+		this.width = 32;
+		this.height = 32;
+		this.x = 0;
+		this.y = 0;
+
+		this.baseSpeed = 3;
+
+		this.movement = {
+			timeSinceLastUpdate: 0,
+			timeToNextUpdate: 1000, // 1000 milliseconds = 1 second
+			x: {
+				direction: 1,
+				speed: this.baseSpeed,
+			},
+			y: {
+				direction: 1,
+				speed: this.baseSpeed,
+			},
+		};
+	}
+
+	update(elapsedTime) {
+		this.movement.timeSinceLastUpdate += elapsedTime;
+		if (
+			this.movement.timeSinceLastUpdate >= this.movement.timeToNextUpdate
+		) {
+			this.movement.x.direction = Math.random() >= 0.5 ? 1 : -1;
+			this.movement.y.direction = Math.random() >= 0.5 ? 1 : -1;
+
+			this.movement.x.speed = Math.random() * this.baseSpeed;
+			this.movement.y.speed = Math.random() * this.baseSpeed;
+
+			this.movement.timeToNextUpdate = Math.random() * 1000 + 500;
+			this.movement.timeSinceLastUpdate = 0;
+		}
+
+		if (this.x + this.width >= canvas.width) {
+			this.movement.x.direction = -1;
+		}
+		if (this.x <= 0) {
+			this.movement.x.direction = 1;
+		}
+		if (this.y + this.height >= canvas.height) {
+			this.movement.y.direction = -1;
+		}
+		if (this.y <= 0) {
+			this.movement.y.direction = 1;
+		}
+
+		this.x += this.movement.x.speed * this.movement.x.direction;
+		this.y += this.movement.y.speed * this.movement.y.direction;
+	}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = "red";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+}
+
 let player = new Player();
+let m1 = new Monster();
 
+let gameObjects = [player, m1];
 
+let currentTime = 0;
+let lastMonsterAdded = 0;
+const monsterSpawnRate = 1000;
 
+function gameLoop(timestamp) {
+	//console.log(timestamp);
+	// clear off the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	let elapsedTime = Math.floor(timestamp - currentTime);
+	currentTime = timestamp;
 
-    player.x += 1
-    player.update();
-    player.render();
+	lastMonsterAdded += elapsedTime;
+	if (lastMonsterAdded >= monsterSpawnRate) {
+		gameObjects.push(new Monster());
+		lastMonsterAdded = 0;
+	}
 
-    requestAnimationFrame(gameLoop);
+	gameObjects.forEach((o) => {
+		o.update(elapsedTime);
+		o.render();
+	});
+
+	requestAnimationFrame(gameLoop);
 }
 
 requestAnimationFrame(gameLoop);
