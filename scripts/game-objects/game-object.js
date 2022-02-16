9822e5fa59506175b60d300b74ff1f5eaf8d4950
monkey-d-luffy <ktkopp@icloud.com>
2022-02-16 16:07:43 -0700

    hi

---
+++ scripts/game-objects/game-object.js
@@ -0,0 +1,61 @@
+//@ts-check
+import { ctx } from "../canvas.js";
+
+export class GameObject {
+	constructor(w, h) {
+		this.x = 0;
+		this.y = 0;
+		this.width = w;
+		this.height = h;
+		this.fillStyle = "";
+		this.lastLocation = new Location(this.x, this.y);
+	}
+
+	update(elapsedTime) {
+		this.lastLocation.x = this.x;
+		this.lastLocation.y = this.y;
+		//console.log(this.lastLocation);
+	}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = this.fillStyle;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+
+	getBounds() {
+		return new ObjectBounds(this.x, this.y, this.width, this.height);
+	}
+
+	/**
+	 * @param { GameObject } o
+	 */
+	isColliding(o) {
+		let myBounds = this.getBounds();
+		let oBounds = o.getBounds();
+
+		if (myBounds.bottom <= oBounds.top) return undefined;
+		if (myBounds.top >= oBounds.bottom) return undefined;
+		if (myBounds.right <= oBounds.left) return undefined;
+		if (myBounds.left >= oBounds.right) return undefined;
+
+		return this.lastLocation;
+	}
+}
+
+class ObjectBounds {
+	constructor(x, y, w, h) {
+		this.top = y;
+		this.bottom = y + h;
+		this.left = x;
+		this.right = x + w;
+	}
+}
+
+export class Location {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
